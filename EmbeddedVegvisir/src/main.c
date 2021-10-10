73e03c8e96c8505946c14e2536356d1005f7b473
Filip <fvukovic123@gmail.com>
2021-10-10 10:36:15 +0200

    Display Raw Image On IMX8M+

--- EmbeddedVegvisir/src/main.c
+++ EmbeddedVegvisir/src/main.c
@@ -1,107 +1,237 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define INFOLOG_LEN 1024
+
+#define BUFSIZE     1024
+#define ORQA_IN
+#define ORQA_REF
+#define ORQA_OUT
+#define ORQA_NOARGS
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 #define GLFW_INCLUDE_ES32
 #include <GLFW/glfw3.h>
+#include <cglm/cglm.h>
 
-static const GLuint WIDTH = 800;
-static const GLuint HEIGHT = 600;
+#include "gen_sphere.h"
 
+typedef enum{
+    OPENGL_OK           = 0,
+    OPENGL_INIT_ERROR   = -1
+} OpenGLFlags;
 
-static const GLchar* vertex_shader_source =
-    "#version 100\n"
-    "attribute vec3 position;\n"
-    "void main() {\n"
-    "   gl_Position = vec4(position, 1.0);\n"
-    "}\n";
-static const GLchar* fragment_shader_source =
-    "#version 100\n"
-    "void main() {\n"
-    "   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n"
-    "}\n";
-static const GLfloat vertices[] = {
-        0.0f,  0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-    -0.5f, -0.5f, 0.0f,
+const GLuint SCR_WIDTH = 1920;
+const GLuint SCR_HEIGHT = 1080; 
+
+// camera_t attributes
+typedef struct camera_t{
+    vec3 cameraPos;
+    GLfloat fov;
+    versor resultQuat;
+}camera_t;
+
+const GLfloat vertices[] = {
+        // pisitions          // texture coords
+         0.5f,  0.5f, 0.0f,   1.0f, 1.0f, // top right vertex
+         0.5f, -0.5f, 0.0f,   1.0f, 0.0f, // bottom right vertex
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, // bottom left vertex
+        -0.5f,  0.5f, 0.0f,   0.0f, 1.0f  // top left vertex
+};
+const GLuint indices[] = {  
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
 };
 
-GLint common_get_shader_program(const char *vertex_shader_source, const char *fragment_shader_source) {
-    GLchar infoLog[INFOLOG_LEN];
-    GLint fragment_shader;
-    GLint shader_program;
-    GLint success;
-    GLint vertex_shader;
+const GLchar *vertexShaderSource = 
+    "attribute vec3 aPos;\n"
+    "attribute vec2 aTexCoord;\n"
+    "varying vec2 TexCoord;\n"
+    "uniform mat4 model;\n"
+    "uniform mat4 view;\n"
+    "uniform mat4 proj;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = proj*view*model*vec4(aPos.x, aPos.y, aPos.z , 1.);\n" // local space to clip space
+    "   TexCoord = vec2(1. - aTexCoord.x, aTexCoord.y);\n" // mirror textures for inside sphere
+    "}\n\0";
 
-    /* Vertex shader */
-    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
-    glCompileShader(vertex_shader);
-    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(vertex_shader, INFOLOG_LEN, NULL, infoLog);
-        printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n%s\n", infoLog);
-    }
+const GLchar *fragmentShaderSource = 
+    "precision mediump float;\n"
+    "varying vec2 TexCoord;\n"
+    "uniform sampler2D texture1;\n" 
+    "void main()\n"
+    "{\n"
+    "   gl_FragColor = texture2D(texture1, TexCoord);\n" 
+    "}\n\0";
 
-    /* Fragment shader */
-    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
-    glCompileShader(fragment_shader);
-    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(fragment_shader, INFOLOG_LEN, NULL, infoLog);
-        printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n%s\n", infoLog);
-    }
+static int orqa_GLFW_init(ORQA_NOARGS void);
+static void orqa_framebuffer_size_callback(ORQA_REF GLFWwindow *window,ORQA_IN GLint width,ORQA_IN GLint height);
 
-    /* Link shaders */
-    shader_program = glCreateProgram();
-    glAttachShader(shader_program, vertex_shader);
-    glAttachShader(shader_program, fragment_shader);
-    glLinkProgram(shader_program);
-    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shader_program, INFOLOG_LEN, NULL, infoLog);
-        printf("ERROR::SHADER::PROGRAM::LINKING_FAILED\n%s\n", infoLog);
-    }
-
-    glDeleteShader(vertex_shader);
-    glDeleteShader(fragment_shader);
-    return shader_program;
-}
 
 int main(void) {
-
-    glfwInit();
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, __FILE__, NULL, NULL);
+    if (orqa_GLFW_init()) return OPENGL_INIT_ERROR;
+    glfwWindowHint(GLFW_DECORATED, GLFW_FALSE); // Full screen
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Vegvisir Project", NULL, NULL); // glfw window object creation
+    if (window == NULL){
+        fprintf(stderr, "In file: %s, line: %d Failed to create GLFW window\n", __FILE__, __LINE__);
+        glfwTerminate();
+        return OPENGL_INIT_ERROR;
+    }
     glfwMakeContextCurrent(window);
 
-    printf("GL_VERSION  : %s\n", glGetString(GL_VERSION) );
+    glfwSetFramebufferSizeCallback(window, orqa_framebuffer_size_callback); // manipulate view port
 
-    GLuint shader_program = common_get_shader_program(vertex_shader_source, fragment_shader_source);
-    GLuint pos = glGetAttribLocation(shader_program, "position");
+    // shader init, compilation and linking
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER); 
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    GLuint shaderProgram = glCreateProgram();
+    GLint success;
+    GLchar infoLog[BUFSIZE];
 
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glViewport(0, 0, WIDTH, HEIGHT);
-    GLuint VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(pos, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0);
-    glEnableVertexAttribArray(pos);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glUseProgram(shader_program);
-
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-        glClear(GL_COLOR_BUFFER_BIT);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glfwSwapBuffers(window);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success){
+        glGetShaderInfoLog(vertexShader, BUFSIZE, NULL, infoLog);
+        fprintf(stderr, "In file: %s, line: %d ERROR::SHADER::VERTEX::COMPILATION_FAILED\nError:\n%s\n", __FILE__, __LINE__, infoLog);
+        goto shaderError; 
     }
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success){
+        glGetShaderInfoLog(fragmentShader, BUFSIZE, NULL, infoLog);
+        fprintf(stderr, "In file: %s, line: %d ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\nError:\n%s\n", __FILE__, __LINE__, infoLog);
+        goto shaderError;
+    }
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, BUFSIZE, NULL, infoLog);
+        fprintf(stderr, "In file: %s, line: %d ERROR::SHADER::PROGRAM::LINKING_FAILED\nError:\n%s\n", __FILE__, __LINE__, infoLog);
+        goto linkingError; 
+    } 
+
+    // init & binding array & buffer objects
+    GLuint VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO); 
+    glGenBuffers(1, &EBO);
+    glBindVertexArray(VAO); 
+    glBindBuffer(GL_ARRAY_BUFFER , VBO);
+    glBufferData(GL_ARRAY_BUFFER , sizeof(vertices), vertices, GL_STATIC_DRAW );
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER , EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER , sizeof(indices), indices, GL_STATIC_DRAW );
+
+    // get indexes for shader variables
+    GLuint posLoc = glGetAttribLocation(shaderProgram, "aPos");
+    GLuint texLoc = glGetAttribLocation(shaderProgram, "aTexCoord");
+    glVertexAttribPointer(posLoc, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (float*)0);
+    glEnableVertexAttribArray(posLoc);
+    glVertexAttribPointer(texLoc, 2, GL_FLOAT, GL_FALSE,  5 * sizeof(float), (void*)(3* sizeof(float)));
+    glEnableVertexAttribArray(texLoc);
+
+    // texture init
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    // camera init
+    camera_t cam;
+    cam.cameraPos[0] = 0.0f; cam.cameraPos[1] = 0.0f; cam.cameraPos[2] = 0.0f;
+    cam.resultQuat[0] = 0.0f; cam.resultQuat[1] = 0.0f; cam.resultQuat[2] = 0.0f; cam.resultQuat[3] = 1.0f;
+    cam.fov = 4.8f;
+
+    // loading image!
+    GLuint width, height, nrChannels;
+    unsigned char *data = stbi_load("./data/image", &width, &height, &nrChannels, 0); 
+    if (data){
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        stbi_image_free(data);
+    } else{
+        fprintf(stderr, "In file: %s, line: %d Failed to load texture\n", __FILE__, __LINE__);
+        goto loadError;
+    } 
+
+    // MVP matrices init
+    mat4 model, proj, view;
+    glm_mat4_identity(model); glm_mat4_identity(view); glm_mat4_identity(proj);
+
+    // get MVP shader indexes
+    GLuint modelLoc = glGetUniformLocation(shaderProgram, "model");
+    GLuint viewLoc = glGetUniformLocation(shaderProgram, "view");
+    GLuint projLoc = glGetUniformLocation(shaderProgram, "proj");
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+    glUseProgram(shaderProgram);
+
+    while (!glfwWindowShouldClose(window)){ // render loop
+        // render
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT); // | GL_DEPTH_BUFFER_BIT);
+
+        // generate projection matrix
+        // glm_perspective(cam.fov, (GLfloat)SCR_WIDTH / (GLfloat)SCR_HEIGHT, 0.01f, 100.0f, proj); // zoom
+
+        // generate view matrix
+        // glm_quat_look(cam.cameraPos, cam.resultQuat, view);
+
+        // send MVP matrices to vertex shader
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, &model[0][0]); 
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]); 
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, &proj[0][0]); 
+
+        // build texture  
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        // draw
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        // glfw: swap buffers and poll IO events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    // deallocating stuff
+    loadError:
+
+    threadError:
+    glDeleteVertexArrays(1, &VAO); 
     glDeleteBuffers(1, &VBO);
-    glfwTerminate();
-    return EXIT_SUCCESS;
+    glDeleteBuffers(1, &EBO);
+    glDeleteTextures(1, &texture);
+    linkingError:
+    glDeleteProgram(shaderProgram);
+    shaderError:
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    glfwTerminate(); // glfw: terminate, clearing all previously allocated GLFW resources.
+    return OPENGL_OK;
+
 }
+
+
+/// This function initializes GLFW.
+/// Returns OPENGL_OK on success and OPENGL_INIT_ERROR on failure.
+static int orqa_GLFW_init(ORQA_NOARGS void){ 
+    // glfw: we first initialize GLFW with glfwInit, after which we can configure GLFW using glfwWindowHint
+    if(!glfwInit()){
+        fprintf(stderr, "In file: %s, line: %d Failed to initialize GLFW\n", __FILE__, __LINE__);
+        glfwTerminate();
+        return OPENGL_INIT_ERROR;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1); 
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+
+    return OPENGL_OK;
+}
+/// This callback function keeps track of window size and updates it when needed.
+static void orqa_framebuffer_size_callback(ORQA_REF GLFWwindow *window,ORQA_IN GLint width,ORQA_IN GLint height){
+    glViewport(0, 0, width, height); // size of the rendering window
+}
+
