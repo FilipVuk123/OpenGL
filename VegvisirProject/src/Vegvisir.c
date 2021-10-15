15503869c24b8166878c6c506657c9a840115067
FilipVuk123 <fvukovic123@gmail.com>
2021-10-15 14:03:08 +0200

    MRSS sphere implemented!

--- VegvisirProject/src/Vegvisir.c
+++ VegvisirProject/src/Vegvisir.c
@@ -52,7 +52,7 @@ typedef struct camera_t{
 }camera_t;
 
 pthread_mutex_t mutexLock;
-
+/*
 const GLchar *vertexShaderSource = 
     "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
@@ -65,6 +65,35 @@ const GLchar *vertexShaderSource =
     "{\n"
     "   gl_Position = proj*view*model*vec4(aPos.x, aPos.y, aPos.z , 1.);\n" // local space to clip space
     "   TexCoord = vec2(1. - aTexCoord.x, aTexCoord.y);\n" // mirror textures for inside sphere
+    "}\n\0";*/
+
+// MRSS shader
+const GLchar *vertexShaderSource = 
+    "attribute vec3 aPos;\n"
+    "attribute vec2 aTexCoord;\n"
+    "varying vec2 TexCoord;\n"
+    "float newMin = 0.;\n"
+    "float newMax = 1.;\n"
+    "float oldMin = 0.25;\n"
+    "float oldMax = 0.75;\n"
+    "float oldRange = oldMax - oldMin;\n"
+    "float newRange = newMax - newMin;\n"
+    "float newValueX;\n"
+    "float newValueY;\n"
+    "uniform mat4 model;\n"
+    "uniform mat4 view;\n"
+    "uniform mat4 proj;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = proj*view*model*vec4(aPos.x, aPos.y, aPos.z , 1.);\n" // local space to clip space
+    "   if ((1. - aTexCoord.x) < oldMin || (1. - aTexCoord.x) > oldMax || aTexCoord.y < oldMin || aTexCoord.y > oldMax){\n"
+    "       newValueX = -1.;\n"
+    "       newValueY = -1.;\n"
+    "   }else{\n"
+    "       newValueX = ( ((1. - aTexCoord.x) - oldMin) * newRange / oldRange ) + newMin;\n"
+    "       newValueY = ( (aTexCoord.y - oldMin) * newRange / oldRange ) + newMin;\n"
+    "   }\n"
+    "   TexCoord = vec2(newValueX, newValueY);\n" // mirror textures for inside sphere
     "}\n\0";
 
 const GLchar *fragmentShaderSource = 
@@ -99,7 +128,7 @@ int main(){
     glfwSetFramebufferSizeCallback(window, orqa_framebuffer_size_callback); // manipulate view port
     glfwSetCursorPosCallback(window, orqa_mouse_callback); // move camera_t with cursor
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED); // use cursor but do not display it
-    // glfwSetScrollCallback(window, orqa_scroll_callback); // zoom in/out using mouse wheel
+    glfwSetScrollCallback(window, orqa_scroll_callback); // zoom in/out using mouse wheel
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){ // glad: load all OpenGL function pointers. GLFW gives us glfwGetProcAddress that defines the correct function based on which OS we're compiling for
         fprintf(stderr, "In file: %s, line: %d Failed to create initialize GLAD\n", __FILE__, __LINE__);
@@ -176,12 +205,12 @@ int main(){
     camera_t cam;
     cam.cameraPos[0] = 0.0f; cam.cameraPos[1] = 0.0f; cam.cameraPos[2] = 0.0f;
     cam.resultQuat[0] = 0.0f; cam.resultQuat[1] = 0.0f; cam.resultQuat[2] = 0.0f; cam.resultQuat[3] = 1.0f;
-    cam.fov = 4.8f;
+    cam.fov = 5.2f;
     glfwSetWindowUserPointer(window, &cam); // sent camera object to callback functions
 
     // TCP thread & mutex init
     pthread_t tcp_thread;
-    pthread_create(&tcp_thread, NULL, orqa_tcp_thread, &cam);
+    // pthread_create(&tcp_thread, NULL, orqa_tcp_thread, &cam);
     if (pthread_mutex_init(&mutexLock, NULL) != 0) {
         fprintf(stderr, "Mutex init has failed! \n");
         goto threadError;
@@ -189,31 +218,28 @@ int main(){
 
     // loading video file!
     // Before loading generate RGB: $ ffmpeg -y -i input.mp4 -c:v libx264rgb output.mp4
-    video_reader_t vr_state;  
+    video_reader_t vr_state;
+    /*  
     if(orqa_video_reader_open_file(&vr_state, "../data/360videoRGB.mp4")){
         printf("Could not open file\n");
         goto loadError;
     }
     const GLuint width = vr_state.width;  const GLuint height = vr_state.height;
-    fprintf(stderr, "%d, %d\n", width, height);
+    fprintf(stderr, "%d, %d\n", width, height);*/
     
-    /*
+    
     // loading image!
     GLuint width, height, nrChannels;
-    // unsigned char *data = stbi_load("../data/result0.jpg", &width, &height, &nrChannels, 0); 
-    // unsigned char *data = stbi_load("../data/panorama1.bmp", &width, &height, &nrChannels, 0); 
-    // unsigned char *data = stbi_load("../data/result2.jpg", &width, &height, &nrChannels, 0); 
-    // unsigned char *data = stbi_load("../data/earth.jpg", &width, &height, &nrChannels, 0); 
-    unsigned char *data = stbi_load("../data/360test.jpg", &width, &height, &nrChannels, 0); 
+    unsigned char *data = stbi_load("../data/output0.png", &width, &height, &nrChannels, 0); 
     if (data){
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
         stbi_image_free(data);
     } else{
         fprintf(stderr, "In file: %s, line: %d Failed to load texture\n", __FILE__, __LINE__);
         goto loadError;
     } 
-    */
+    
 
     // MVP matrices init
     mat4 model, proj, view;
@@ -246,11 +272,12 @@ int main(){
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]); 
         glUniformMatrix4fv(projLoc, 1, GL_FALSE, &proj[0][0]); 
         
+        /*
         // get video frame -> generate texture
         uint8_t *frame = orqa_video_reader_read_frame(&vr_state);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, frame); 
         glGenerateMipmap(GL_TEXTURE_2D);
-        free (frame);
+        free (frame);*/
 
         // build texture  
         glBindTexture(GL_TEXTURE_2D, texture);
@@ -320,8 +347,8 @@ static void orqa_framebuffer_size_callback(ORQA_REF GLFWwindow *window,ORQA_IN G
 static void orqa_scroll_callback(ORQA_REF GLFWwindow *window, ORQA_IN GLdouble xoffset, ORQA_IN GLdouble yoffset){
     camera_t *cam = glfwGetWindowUserPointer(window);	
     cam->fov -= (GLfloat)yoffset/5; // update fov
-    // cap fov in between 4.8 and 6.2
-    if (cam->fov < 4.8f) cam->fov = 4.8f;
+    // cap fov in between 5.2 and 6.2
+    if (cam->fov < 5.2f) cam->fov = 5.2f;
     if (cam->fov > 6.2f) cam->fov = 6.2f;   
 }
 
